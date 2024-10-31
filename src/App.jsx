e2af97f11ce11a29972768c2d0f6760c53b5b797
destylae <destypati1122@gmail.com>
2024-10-31 09:05:22 +0700

    pertemuan2

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,22 @@
 import React from "react";
 
-// Class Komponen
-class Button extends React.Component {
-  render() {
-    return (
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-white text-black"
-        type="submit"
-      >
-        Beli Sekarang
-      </button>
-    );
-  }
-}
-
-// Functional Komponen
-function ButtonBlack() {
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props;
   return (
-    <button
-      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-      type="submit"
-    >
-      Beli Sekarang
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {children}
     </button>
   );
-}
-
-function ButtonRed() {
-  return (
-    <button
-      className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
-      type="submit"
-    >
-      Beli Sekarang
-    </button>
-  );
-}
+};
 
 function App() {
   return (
-    <div className="flex justify-center bg-blue-400 min-h-screen items-center">
-      <div>
-        <Button />
-        <Button />
-        <ButtonRed />
-        <ButtonBlack />
+    <div className="flex justify-center bg-cyan-100 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-green-300">Login</Button>
+        <Button variant="bg-blue-400">Logout</Button>
+        <Button></Button>
+        <Button variant="bg-red-500">Submit</Button>
       </div>
     </div>
   );
